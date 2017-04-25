56cb2abec724a7f50a5a512ee7cdce83c482323e
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-25 16:10:46 +0100

    Add idea of pre/post potentiation/depression to STDP

--- learning/stdp.py
+++ learning/stdp.py
@@ -8,10 +8,14 @@ class STDP(LearningMethod):
   def __init__(self):
     self.adjustment = 0.1
 
+  def update_weights(self, neuron, adjustment):
+    adjustments = np.multiply(neuron.inputs, adjustment)
+    signed_adjustments = np.multiply(adjustments, np.sign(neuron.weights))
+    adjusted = np.add(neuron.weights, signed_adjustments)
+    neuron.weights = adjusted
+
   def update(self, layers):
     for layer in layers:
       for neuron in layer:
-        adjustments = np.multiply(neuron.inputs, self.adjustment)
-        signed_adjustments = np.multiply(adjustments, np.sign(neuron.weights))
-        adjusted = np.add(neuron.weights, signed_adjustments)
-        neuron.weights = adjusted
+        correlated_adjustment = self.adjustment if neuron.refractoryTime is 0 else -self.adjustment
+        self.update_weights(neuron, correlated_adjustment)
