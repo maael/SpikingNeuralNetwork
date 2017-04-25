95a7d57b99cf9d29fe7fda396e18bbe4e4f00f62
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-25 16:29:59 +0100

    Update example file

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from neurons.LeakyIntegrateAndFireNeuron import LeakyIntegrateAndFireNeuron
 from learning.stdp import STDP
-from snn import SNN
+from network.snn import SNN
 
 net = SNN(3, [2, 4], 6, LeakyIntegrateAndFireNeuron, STDP())
+print(net.layers[0][0].weights)
+print('Final', net.solve([ 1, 1, 1]))
+print(net.layers[0][0].weights)
+print('Final', net.solve([ 1, 1, 1]))
 print(net.layers[0][0].weights)
 print('Final', net.solve([ 1, 1, 1]))
 print(net.layers[0][0].weights)
