53f581f6389452c0293dfdd65e96ed600d027557
Matthew Elphick <matt.a.elphy@gmail.com>
2017-04-25 11:56:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Module information
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+  name='SpikingNeuralNetwork',
+  version='1.0',
+  description='A configurable simple spiking neural network model',
+  url='https://github.com/maael/SpikingNeuralNetwork',
+  download_url='https://github.com/maael/SpikingNeuralNetwork',
+  author='Matthew Elphick',
+  author_email='matt.a.elphy@gmail.com',
+  packages=find_packages())
