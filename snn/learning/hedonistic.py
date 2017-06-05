3a10dd29d5525b5a0f6089c5d35085357aa11209
Matthew Elphick <matt.a.elphy@gmail.com>
2017-06-05 14:14:13 +0100

    Another attempt at the hedonistic method

--- snn/learning/hedonistic.py
+++ snn/learning/hedonistic.py
@@ -1,11 +1,25 @@
-from snn.learning.method import LearningMethod
+from snn.learning.stdp import STDP
 
 """
 Hedonistic variant of STDP Learning Method
 """
-class HedonisticSTDP(LearningMethod):
+class HedonisticSTDP(STDP):
   def __init__(self):
-    self.adjustment = 0.1
+    self.tau_c = 1
+    self.tau_d = 0.2
+
+  def calculate_c(self):
+    self.c = (self.c / self.tau_c) + (STDP(self.tau) * (delta))
+    return self.c
+
+  def calculate_s(self):
+    return self.c * self.d
+
+  def calculate_d(self):
+    self.d = (self.d / self.tau_d) + self.dopamine(t)
+
+  def dopamine(self, t):
+    return 1
 
   def update(self, layers):
-    neuron.weights = neuron.weights
+    super(HedonisticSTDP, self).update(layers)
